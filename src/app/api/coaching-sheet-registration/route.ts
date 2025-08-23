4c79b227f207d8f26045179d6b67a493c79732e9
Cristian Rosca <cristianrrosca@gmail.com>
2025-08-23 16:48:13 +0100

    add route update

--- src/app/api/coaching-sheet-registration/route.ts
+++ src/app/api/coaching-sheet-registration/route.ts
@@ -1,12 +1,10 @@
-import { BasicContactEmail } from "@/app/landing-page/BasicContactEmail";
 import { Resend } from "resend";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextResponse } from "next/server";
-import GoogleDriveAccessEmail from "@/app/components/email/GoogleDriveLinkEmail";
 import { NOTIFICATION_RECIPIENT_EMAIL } from "@/app/util/constants";
+import { BasicContactEmail } from "@/app/email/BasicContactEmail";
 
 const FREE_TIER_REGISTRATION_HEADER = "CoachPal Free Tier Registration 📊";
-const FREE_TIER_REGISTRATION_SUBHEADER = "New"
 const PREMIUM_TIER_REGISTRATION_HEADER = "CoachPal Premium Tier Registration 🚀";
 
 const resend = new Resend(process.env.RESEND_API_KEY!);
@@ -34,13 +32,13 @@ export async function POST(req: Request) {
   const { email, name, communicationConsent, isPremium } = body;
 
   try {
-    const { error: ownEmailError } = await resend.emails.send({
+    const { data, error: ownEmailError } = await resend.emails.send({
       from: "CoachPal <info@coachpal.io>",
       to: NOTIFICATION_RECIPIENT_EMAIL,
       subject: isPremium
         ? PREMIUM_TIER_REGISTRATION_HEADER
         : FREE_TIER_REGISTRATION_HEADER,
-      react: BasicContactEmail({
+      react: await BasicContactEmail({
         heading: "Client Registration Details",
         email,
         name,
@@ -57,16 +55,7 @@ export async function POST(req: Request) {
       );
     }
     
-    const { data, error } = await resend.emails.send({
-      from: "CoachPal <info@coachpal.io>",
-      to: email,
-      subject: "Your Coaching Tools & Systems 🚀",
-      react: GoogleDriveAccessEmail({ name, variant: isPremium ? "premium" : "free" }),
-    });
-  
-    if (error) {
-      return NextResponse.json({ error }, { status: 500 });
-    }
+
 
     return NextResponse.json(data);
   } catch (error) {
